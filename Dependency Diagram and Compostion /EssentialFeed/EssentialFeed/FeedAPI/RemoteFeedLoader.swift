1c6cdee25ddc6a4da18bdb035820034818afea58
Jerry Webb <ljwebbdev8800@gmail.com>
2021-06-04 16:58:00 -0500

    Delivers invalidData error on 200 HTTP response with Invalid json.

--- Dependency Diagram and Compostion /EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ Dependency Diagram and Compostion /EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum HTTPClientResult {
-    case success(HTTPURLResponse)
+    case success(Data, HTTPURLResponse)
     case failure(Error)
 }
 
@@ -36,7 +36,7 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
                 case .success:
-                    completion(.invalidData)
+//                    completion(.invalidData)
                 case .failure:
                     completion(.connectivity)
             }
--- Dependency Diagram and Compostion /EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ Dependency Diagram and Compostion /EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -61,11 +61,19 @@ class RemoteFeedLoaderTests: XCTestCase {
             client.complete(withStatusCode: code, at: index)
             
             XCTAssertEqual(capturedError, [.invalidData])
-            
-            
         }
-
+    }
+    
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        var capturedError = [RemoteFeedLoader.Error]()
         
+        sut.load { capturedError.append($0) }
+                
+        let invalidJSON = Data(bytes: "invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedError, [.invalidData])
     }
     
     
@@ -92,14 +100,14 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int,at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code,
                                            httpVersion: nil,
                                            headerFields: nil
             )!
             
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }
