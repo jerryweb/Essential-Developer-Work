da621acea37e4ee481419aa4407c35ef46e7fbc8
Jerry Webb <ljwebbdev8800@gmail.com>
2021-06-08 21:31:42 -0500

    delivers items array on 200 HTTP response with JSON items.

--- Dependency Diagram and Compostion /EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
+++ Dependency Diagram and Compostion /EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -14,4 +14,19 @@ public struct FeedItem: Equatable {
     let location: String?
     let imageUrl: URL
     
+    public init(id: UUID, description: String?, location: String?, imageUrl: URL){
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageUrl = imageUrl
+    }
+}
+
+extension FeedItem: Decodable{
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageUrl = "image"
+    }
 }
